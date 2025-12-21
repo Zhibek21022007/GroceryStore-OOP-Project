5e3606de0e64621a28111b3aec472d8de0a7a4a2
Zhibek <253773@astanait.edu.kz>
2025-12-21 19:47:51 +0500

    Initial commit: add Grocery Store Management project setup

---
+++ src/com/zhkgrocerystore/management/Main.java
@@ -0,0 +1,13 @@
+package com.zhkgrocerystore.management;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Grocery Store Management System!");
+        System.out.println("This system is designed to simplify daily grocery store operations.");
+        System.out.println("It helps manage product inventory and stock levels.");
+        System.out.println("The system records sales transactions and generates bills.");
+        System.out.println("It also supports reporting to improve store efficiency.");
+    }
+}
+
